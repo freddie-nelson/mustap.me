0a3af5077065137ecd6f2e68f8a3aed1f7a71750
Freddie Nelson <freddie0208@hotmail.com>
2020-03-17 17:04:11 +0000

    ui animations added to navbar

--- src/components/TheHome/components/TheSearchbox.vue
+++ src/components/TheHome/components/TheSearchbox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search-box">
-      <svg @click="searched" xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="none">
+      <svg @click="searched" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none">
         <path fill-rule="evenodd" d="M18.406 16.624l6.725 6.725a1.26 1.26 0 0 1-.001 1.783 1.26 1.26 0 0 1-1.783-.001l-6.725-6.725a10 10 0 0 1-13.627-1.295 10 10 0 0 1 .432-13.682 10 10 0 0 1 13.681-.432 10 10 0 0 1 1.296 13.627h.001zM10.5 18a7.5 7.5 0 0 0 7.5-7.5A7.5 7.5 0 0 0 10.5 3 7.5 7.5 0 0 0 3 10.5a7.5 7.5 0 0 0 7.5 7.5z" fill="#fff"/>
       </svg>
       <div class="search-box__seperator"></div>
@@ -26,7 +26,12 @@ export default {
         align-items: center;
         
         svg {
-          cursor: pointer;
+            cursor: pointer;
+            transition: all .2s ease-out;
+
+            &:hover {
+                transform: scale(1.1);
+            }
         }
 
         &__seperator {
@@ -43,9 +48,18 @@ export default {
             border: none;
             width: 100%;
             height: 40px;
-            font-size: 32px;
+            font-size: 30px;
             outline: none !important;
             color: white;
+
+            &:focus::placeholder {
+                opacity: 0;
+            }
+
+            &::placeholder {
+                transition: opacity .4s ease-in;
+                color: rgba(255, 255, 255, 0.185);
+            }
         }
     }
 </style>
