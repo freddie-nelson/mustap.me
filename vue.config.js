276e0d8dad76431ee8649cb509a6e72be7731b58
Freddie Nelson <freddie0208@hotmail.com>
2020-09-10 22:58:45 +0100

    updated electron to 10.1.1

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,19 @@
 module.exports = {
+  configureWebpack: {
+    target: "electron-renderer"
+  },
   pluginOptions: {
     electronBuilder: {
       builderOptions: {
         appId: "Mustap",
         linux: {
           icon: "public/512x512.png",
-          target: ["AppImage", "snap"]
+          target: ["AppImage", "snap"],
         },
         win: {
-          icon: "public/icon.ico"
-        }
-      }
-    }
-  }
+          icon: "public/icon.ico",
+        },
+      },
+    },
+  },
 };
